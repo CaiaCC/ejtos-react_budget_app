fa577dd1bad4f4f0ac1ab9b7562bc9838796a247
Caia Chuang <caiachuang@gmail.com>
2023-11-17 11:10:49 -0800

    added AllocationForm component and its functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 
 const App = () => {
-    return (
-        <AppProvider>
+    return (        <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
@@ -28,11 +28,9 @@ const App = () => {
                         <div className="col-sm">
                             <ExpenseItem />
                         </div>
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }
-
+                    <div className="col-sm">
+                        <AllocationForm />
+                    </div>
                 </div>
             </div>
         </AppProvider>
