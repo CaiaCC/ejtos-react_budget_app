4c64b9e0989ea63f7ea21cb4d8b1e35370de6961
Caia Chuang <caiachuang@gmail.com>
2023-11-17 11:04:54 -0800

    added Remaining component and its functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
-
-// Add code to import the other components here under
-
-
+import Remaining from './components/Remaining';
 import { AppProvider } from './context/AppContext';
 
 const App = () => {
@@ -13,13 +10,13 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        <div className="col-sm">
+                        <div className="col-sm">
                             <Budget />
                         </div>
 
-                        {
-                            /* Add Remaining component here*/
-                        }
+                        <div className="col-sm">
+                            <Remaining />
+                        </div>
 
                         {
                             /* Add ExpenseTotal component here */
