9da1998de63836e00bb1e2d3496e0d02ddaa2685
Caia Chuang <caiachuang@gmail.com>
2023-11-17 14:46:09 -0800

    added currency to budget allocation

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -5,19 +5,17 @@ import { AppContext } from '../context/AppContext';
 const CurrencyDropdown = () => {
     const { dispatch } = useContext(AppContext);
 
-    const changeCurrency = (event) => {
-        console.log(event)
+    const changeCurrency = (value) => {
         dispatch({
             type: 'CHG_CURRENCY',
-            payload: event.target.value,
+            payload: value,
         });
     }
-
 
     return (
         <div className='alert alert-secondary' style={{backgroundColor: '#92e499'}}>
             <label style={{ backgroundColor: '#92e499', color: 'white' }} >Currency
-                <select onChange={(event) => changeCurrency(event.target.value)} style={{ marginLeft: '1rem', backgroundColor: '#92e499', color: 'white' }}>
+                <select className="selecpicker" onChange={(event) => changeCurrency(event.target.value)} style={{ marginLeft: '1rem', backgroundColor: '#92e499', color: 'white' }}>
                     <option style={{ backgroundColor: '#92e499', color: 'black' }} value="£">£ Pound</option>
                     <option style={{ backgroundColor: '#92e499', color: 'black' }} value="$">$ Dollar</option>
                     <option style={{ backgroundColor: '#92e499', color: 'black' }} value="€">€ Euro</option>
